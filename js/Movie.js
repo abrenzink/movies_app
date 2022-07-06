f1dcd7f91fe76e3fb1c2005f05f154175bc02480
abrenzink <a.brenzink@gmail.com>
2022-07-06 16:56:51 -0300

    -

--- js/Movie.js
+++ js/Movie.js
@@ -13,6 +13,7 @@ export default class Movie{
         card.querySelector("img").src = this.poster;
         card.querySelector(".movie-title").textContent = this.title;
         card.querySelector(".movie-year").textContent = this.year;
+        card.querySelector(".movie-type").textContent = this.type;
         parent.appendChild(card);
     }
 }
